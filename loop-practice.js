04a75107116f2d4b47247b6a930b6b88865dff3a
anyavera <neverova.anya@gmail.com>
2020-11-11 23:22:31 -0800

    function: "pick a flower"
    practicing a while loop

---
+++ loop-practice.js
@@ -0,0 +1,58 @@
+var location1 = 'Alkai';
+var location2 = 'Green Lake';
+
+function changeLocation() {
+	console.log('I will be' + location1 + ' swimming with sea lions')
+	
+}
+else {
+	
+	console.log('I will be' + location2 + 'jogging');
+}
+
+changeLocation()
+
+
+
+
+if (weather == sunny) {
+	console.log('bring sun glasses');
+}
+
+
+function getDressed(weather) {
+	if (weather >= 80) {
+		return 'put sunscreen'
+	}
+	else if (weather >= 40) {
+		return 'wear coat'
+	}
+	else {
+		return 'stay home'
+	}
+}
+
+getDressed(80)
+
+var RedFlower = "Here is a flower!";
+let
+function goFindAFlower() {
+while (i === RedFlower){
+	i++;
+}
+}	
+console.log(RedFlower)
+
+let flower = [ 'orange', 'yellow', 'magento', 'red'];
+let i = 0;
+function pickAFlower() {
+	while (i < flower.length) {
+		// which flower is red?
+		if (flower[i] = 'red') {
+			console.log(flower[i]); // find flower in given array
+			// to start aldo goinng next step
+
+		}
+		i ++;
+	}
+}
